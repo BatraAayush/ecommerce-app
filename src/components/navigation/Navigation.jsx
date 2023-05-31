f3fc9bc9e38cb4976c2be6421f3f5f5dad952dd1
BatraAayush <67547222+BatraAayush@users.noreply.github.com>
2023-05-31 21:24:24 +0530

    add responsive navbar

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -5,37 +5,125 @@ import { useLoginContext } from "../../contexts/LoginProvider";
 import { FaShoePrints } from "react-icons/fa";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { AiOutlineHeart } from "react-icons/ai";
-
-
-
 import { useCartContext } from "../../contexts/CartProvider";
 
 export const Navigation = () => {
     const { login, userName } = useLoginContext();
     const { searchHandler } = useDataContext();
-    const { cart, wishlist} = useCartContext();
+    const { cart, wishlist } = useCartContext();
 
     return (
-        <div className="nav-bar">
-            <NavLink className={"heading-link"} to={"/"}>
-                <strong><FaShoePrints /> Shoes Mart</strong>
-            </NavLink>
-            &emsp;
-            <input
-                className="search-bar"
-                onChange={(e) => searchHandler(e)}
-                placeholder="Search"
-                type="text"
-            />
-            &emsp;
-            <div className="links">
-                <NavLink style={({isActive}) => ({fontWeight: isActive ? "bold" : "inherit"})} className={"text-link"} to={"/products"}>Products</NavLink>
-                <NavLink style={({isActive}) => ({fontWeight: isActive ? "bold" : "inherit"})} className={"text-link icon"} to={"/cart"}><AiOutlineShoppingCart/><span className="item-no">{cart.length}</span></NavLink>
-                <NavLink style={({isActive}) => ({fontWeight: isActive ? "bold" : "inherit"})} className={"text-link icon"} to={"/wishlist"}><AiOutlineHeart/><span className="item-no">{wishlist.length}</span></NavLink>
-                <NavLink style={({isActive}) => ({fontWeight: isActive ? "bold" : "inherit"})} className={"text-link"} to={"/login"}>{login ? "Logout" : "Login"}</NavLink>
-                &emsp;
+        <>
+            <div className="nav-bar desktop">
+                <NavLink className={"heading-link"} to={"/"}>
+                    <strong>
+                        <FaShoePrints /> Shoes Mart
+                    </strong>
+                </NavLink>
+                <input
+                    className="search-bar"
+                    onChange={(e) => searchHandler(e)}
+                    placeholder="Search"
+                    type="text"
+                />
+                <div className="links">
+                    <NavLink
+                        style={({ isActive }) => ({
+                            fontWeight: isActive ? "bold" : "",
+                        })}
+                        className={"text-link"}
+                        to={"/products"}
+                    >
+                        Products
+                    </NavLink>
+                    <NavLink
+                        style={({ isActive }) => ({
+                            fontWeight: isActive ? "bold" : "",
+                        })}
+                        className={"text-link icon"}
+                        to={"/cart"}
+                    >
+                        <AiOutlineShoppingCart />
+                        <span className="item-no">{cart.length}</span>
+                    </NavLink>
+                    <NavLink
+                        style={({ isActive }) => ({
+                            fontWeight: isActive ? "bold" : "",
+                        })}
+                        className={"text-link icon"}
+                        to={"/wishlist"}
+                    >
+                        <AiOutlineHeart />
+                        <span className="item-no">{wishlist.length}</span>
+                    </NavLink>
+                    <NavLink
+                        style={({ isActive }) => ({
+                            fontWeight: isActive ? "bold" : "",
+                        })}
+                        className={"text-link"}
+                        to={"/login"}
+                    >
+                        {login ? "Logout" : "Login"}
+                    </NavLink>
+                </div>
+                {login && <strong>Hello {userName}</strong>}
             </div>
-            {login && <strong>Hello {userName}</strong>}
-        </div>
+
+            <div className="nav-bar mobile">
+                <>
+                    <div className="links">
+                        <NavLink className={"heading-link"} to={"/"}>
+                            <strong>
+                                <FaShoePrints />
+                            </strong>
+                        </NavLink>
+                        <NavLink
+                            style={({ isActive }) => ({
+                                fontWeight: isActive ? "bold" : "",
+                            })}
+                            className={"text-link"}
+                            to={"/products"}
+                        >
+                            Products
+                        </NavLink>
+                        <NavLink
+                            style={({ isActive }) => ({
+                                fontWeight: isActive ? "bold" : "",
+                            })}
+                            className={"text-link icon"}
+                            to={"/cart"}
+                        >
+                            <AiOutlineShoppingCart />
+                            <span className="item-no">{cart.length}</span>
+                        </NavLink>
+                        <NavLink
+                            style={({ isActive }) => ({
+                                fontWeight: isActive ? "bold" : "",
+                            })}
+                            className={"text-link icon"}
+                            to={"/wishlist"}
+                        >
+                            <AiOutlineHeart />
+                            <span className="item-no">{wishlist.length}</span>
+                        </NavLink>
+                        <NavLink
+                            style={({ isActive }) => ({
+                                fontWeight: isActive ? "bold" : "",
+                            })}
+                            className={"text-link"}
+                            to={"/login"}
+                        >
+                            {login ? "Logout" : "Login"}
+                        </NavLink>
+                    </div>
+                    <input
+                        className="search-bar"
+                        onChange={(e) => searchHandler(e)}
+                        placeholder="Search"
+                        type="text"
+                    />
+                </>
+            </div>
+        </>
     );
 };
