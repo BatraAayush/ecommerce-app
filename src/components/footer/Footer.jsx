cb69221298b1799859bddfb761f5c6fe834efd7a
BatraAayush <67547222+BatraAayush@users.noreply.github.com>
2023-06-01 12:59:09 +0530

    add links in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,33 @@
-import { Link } from "react-router-dom"
-import "./footer.css"
+import { Link } from "react-router-dom";
+import "./footer.css";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { CgMail } from "react-icons/cg";
+import { AiFillGithub } from "react-icons/ai";
 
 export const Footer = () => {
-    return <div className="footer">
-        <strong>Shoes Mart</strong>
-        <div>&#169; Developed By Aayush Batra</div>
-        <div className="social-media">
-            <Link>LinkedIn</Link>
-            <Link>Twitter</Link>
-            <Link>Github</Link>
+    return (
+        <div className="footer">
+            <strong>Shoes Mart</strong>
+            <div>&#169; Developed By Aayush Batra</div>
+            <div className="social-media">
+                <Link
+                    to={"https://www.linkedin.com/in/aayush-batra-475ab9201/"}
+                    target="_blank"
+                >
+                    <AiOutlineLinkedin />
+                </Link>
+                <Link
+                    to={
+                        "https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=aayushbatra33@gamil.com"
+                    }
+                    target="blank"
+                >
+                    <CgMail />
+                </Link>
+                <Link to={"https://github.com/BatraAayush"} target="_blank">
+                    <AiFillGithub />
+                </Link>
+            </div>
         </div>
-    </div>
-}
+    );
+};
